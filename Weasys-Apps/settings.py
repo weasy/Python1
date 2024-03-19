2d4a3f800d28b67480e64d46726f99ffc57b7e70
weasy <jim@weasy.net>
2024-03-19 13:09:45 -0600

    Updated template.

--- Weasys-Apps/settings.py
+++ Weasys-Apps/settings.py
@@ -19,7 +19,7 @@ LOGGING = {
     'disable_existing_loggers': False,
     'handlers': {
         'file': {
-            'level': 'DEBUG',
+            'level': 'INFO',
             'class': 'logging.FileHandler',
             'filename': '/home/site/wwwroot/logfile.log',
         },
